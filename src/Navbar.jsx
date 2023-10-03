7f2ec2845344640b79b45e4b146a399d93a5129b
stasholo <stass1994@yandex.ru>
2023-10-03 14:37:56 +0500

    add catFacts

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
         <nav>
+            
     <Link to="/">Home</Link>
       <Link to="/first">First</Link>
       <Link to="/second">Second</Link>
         </nav>
+    
     )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
 const HomePage = () => {
+
+    const [facts, setFacts] = useState([]) 
+
+    useEffect(() => {
+        fetch("https://cat-fact.herokuapp.com/facts")
+        .then((r) => {
+        return r.json() 
+    })
+        .then(r => {
+            setFacts(r)
+        })
+    }, [])
+
+    console.log('facts:', facts)
+
     return (
         <div>
-            <h1> HomePage </h1>
+            <h1> Cat Facts </h1>
+            <div className="catfacts">
+            <h2>
+                {facts.map(facts => (
+                    <li>{facts.text}</li>
+                ))}
+            </h2></div>
         </div>
     )
 }
